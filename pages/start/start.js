3d9580786def738dc7738e9dbb29facd6ac91ada
Chris009 <Christoph.Ueberle@gmx.de>
2015-03-19 14:29:04 +0100

    First Commit

---
+++ pages/start/start.js
@@ -0,0 +1,15 @@
+﻿
+
+function storyNav() {
+    window.location.replace('../story/story.html');
+}
+function miniNav() {
+    window.location.replace('pages/story/story.html');
+}
+function scoresNav() {
+    window.location.replace('pages/highscore/highscore.html');
+}
+function settingsNav() {
+    window.location.replace('pages/settings/settings.html');
+}
+
